726cb42c9406c66ff46e57f5043a00092c564b77
Matthew Gritt <matthew.gritt@yahoo.co.uk>
2022-01-06 16:05:24 +0000

    update

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
 function Projects() {
   return (
     <div className="hover:scale-100">
-      {/* project flex */}
+      {/* project flex 1 */}
       <div className="md:flex items-center text-lg sm:text-xl ">
         {/* ebay */}
         <div className="  font-bold sm:mr-3 w-full mt-10">
@@ -56,7 +56,7 @@ function Projects() {
           </div>
         </div>
       </div>
-      {/* project flex */}
+      {/* project flex 2*/}
       <div className="md:flex items-center text-lg sm:text-xl">
         {/* todo list */}
         <div className="mt-10 font-bold sm:mr-3">
@@ -101,6 +101,59 @@ function Projects() {
             </a>
             <a
               href="https://github.com/MatthewGritt/pairs"
+              target="_blank"
+              className="projectBtn"
+            >
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* project flex 3*/}
+      <div className="md:flex items-center text-lg sm:text-xl">
+        {/* todo list */}
+        <div className="mt-10 font-bold sm:mr-3">
+          <img
+            className=" object-contain"
+            src="/images/twitter.png"
+            alt="facebook pic"
+          />
+          <p className="py-1 text-center bg-gray-100">Twitter clone in react</p>
+          <div className="flex items-center justify-evenly mt-2 ">
+            <a
+              href="https://twitter-clone-weld-seven.vercel.app/"
+              target="_blank"
+              className="projectBtn"
+            >
+              See Live
+            </a>
+            <a
+              href="https://github.com/MatthewGritt/twitter-clone"
+              target="_blank"
+              className="projectBtn"
+            >
+              Source Code
+            </a>
+          </div>
+        </div>
+        {/* pairs */}
+        <div className="mt-10 font-bold sm:ml-3">
+          <img
+            className=" object-contain"
+            src="/images/carousel.png"
+            alt="pairs pic"
+          />
+          <p className="py-1 text-center bg-gray-100">Carousel</p>
+          <div className="flex items-center justify-evenly mt-2 ">
+            <a
+              href="https://matthewgritt.github.io/carousel/"
+              target="_blank"
+              className="projectBtn"
+            >
+              See Live
+            </a>
+            <a
+              href="https://github.com/MatthewGritt/carousel"
               target="_blank"
               className="projectBtn"
             >
