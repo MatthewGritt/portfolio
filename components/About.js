56536235841c13a211e58ce1e38561b94f93ebf0
Matthew Gritt <matthew.gritt@yahoo.co.uk>
2022-04-14 20:52:25 +0100

    update

--- components/About.js
+++ components/About.js
@@ -7,14 +7,14 @@ function About() {
           Hi, My name is Matt, I'm a self-taught Front-End Developer. <br />
           <br />
           Learning to code has been an enjoyable and rewarding experience. I Now
-          feel at a point where I can build multipage websites with my HTML &
+          feel at a point where I can build multi-page websites with my HTML &
           CSS skills. I have also spent time learning and practicing JavaScript
           (including working with API's), functional programming and object
           oriented programming. <br />
           <br /> I am aware that this is a fast growing industry, therefore I
           make sure to learn and use frameworks and tools such as React,
           Tailwind and next js. <br />
-          <br /> I am a developer because I enjoy creating and i'm inspired to
+          <br /> I am a developer because I enjoy creating and I'm inspired to
           be a better me today than I was yesterday.
         </p>
       </div>
